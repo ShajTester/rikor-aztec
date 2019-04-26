058b19537a3a11f17e7b2642bd76ae231f577d49
ShajTester <andrey-shaj@narod.ru>
2019-04-26 16:12:02 +0300

    Первая версия управления включением без сохранения состояния

--- meta-facebook/meta-yosemite/recipes-yosemite/rikcgi/files/sh/power.sh
+++ meta-facebook/meta-yosemite/recipes-yosemite/rikcgi/files/sh/power.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-./server_on.sh
+# ./server_on.sh
+pipe=/tmp/rikbtnd.pipe
+
+echo "switch power" > $pipe
 
 # Вернуть состояние сервера
-echo "on"
+
+read line < $pipe
+
+logger $line
+# if [[ "$line" == 'on' ]]; then
+#     echo '"on"'
+# else
+#     echo '"off"'
+# fi
+
+case $line in
+	on)
+		echo '"on"'
+		;;
+	off)
+		echo '"off"'
+		;;
+	*)
+		echo '"?"'
+		;;
+esac
